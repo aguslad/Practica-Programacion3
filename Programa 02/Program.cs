edb92e5dee6db591ff69c111352e423decaffc8b
Agustina <agusladines@hotmail.com>
2026-04-07 17:33:29 -0300

    Codigo

---
+++ Programa 02/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Programa_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- BLOQUE 1: NÚMEROS ENTEROS ---");
+
+            int numProducto = 55455;
+            Console.WriteLine("El número de producto es: " + numProducto);
+
+            Console.WriteLine("\nPresiona ENTER para cerrar.");
+            Console.ReadLine();
+        }
+    }
+}
