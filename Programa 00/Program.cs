d05d1a7756c2acf709caf8bd7f9ebd9a4b83fe34
Agustina <agusladines@hotmail.com>
2026-04-07 17:33:01 -0300

    Codigo

--- Programa 00/Program.cs
+++ Programa 00/Program.cs
@@ -1,14 +1,14 @@
-﻿namespace Programa_00
+﻿namespace Programa_01
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola Mundo");
-            Console.WriteLine("Hola Mundo");
-            Console.WriteLine("Hola Mundo");
-            Console.WriteLine("Hola Mundo");
-            Console.WriteLine("Hola Mundo");
+            Console.WriteLine("Mi nombre es Agustina Ladines");
+            Console.WriteLine("Tengo 26 años");
+            Console.WriteLine("Estudio programación");
+            Console.WriteLine("En la Universidad Tecnológica Nacional - Facultad Regional Tucumán");
+            Console.WriteLine("Mi propósito es recibirme este año");
         }
     }
 }
