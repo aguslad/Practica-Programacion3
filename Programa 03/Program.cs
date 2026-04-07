39ca40fa24dfaaaa01876d60748e0b1fc4bef158
Agustina <agusladines@hotmail.com>
2026-04-07 17:33:51 -0300

    Codigo

---
+++ Programa 03/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Programa_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- BLOQUE 2: NÚMEROS CON DECIMALES ---");
+
+            double largoProducto = 10.85;
+            Console.WriteLine("   El largo del producto es: " + largoProducto + " centimetros");
+
+            decimal precioProducto = 8999.99m;
+            Console.WriteLine("   El valor del producto es: $" + precioProducto);
+            Console.WriteLine("\nPresiona ENTER para cerrar.");
+            Console.ReadLine();
+
+        }
+    }
+}
