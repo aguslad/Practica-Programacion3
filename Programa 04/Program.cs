acc260a083dd75315d45c0cd392755f4ab69d7b1
Agustina <agusladines@hotmail.com>
2026-04-07 17:34:18 -0300

    Codigo

---
+++ Programa 04/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Programa_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- BLOQUE 3: TEXTO Y CARACTERES ---");
+
+            string nombre = "Agustina";
+            string apellido = "Ladines";
+            Console.WriteLine("   Nombre y apellido: " + nombre + " " + apellido);
+
+            // Carácter único (char) - Comillas simples ''
+            char inicialNombre = 'A';
+            char inicialApellido = 'L';
+            Console.WriteLine("   Inicial nombre: '" + inicialNombre + "'");
+            Console.WriteLine("   Inicial apellido: '" + inicialApellido + "'");
+
+
+            Console.WriteLine("\nPresiona ENTER para cerrar.");
+            Console.ReadLine();
+        }
+    }
+}
