e714bc9fbf73a1d4980e3cab5a6c3aac7616116b
Agustina <agusladines@hotmail.com>
2026-04-07 13:33:43 -0300

    Codigo

---
+++ Programa 00/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Programa_00
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo");
+            Console.WriteLine("Hola Mundo");
+            Console.WriteLine("Hola Mundo");
+            Console.WriteLine("Hola Mundo");
+            Console.WriteLine("Hola Mundo");
+        }
+    }
+}
