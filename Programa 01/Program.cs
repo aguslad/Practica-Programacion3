7532e1d9db7b30d4352b6d2bb8951f68ad8613c1
Agustina <agusladines@hotmail.com>
2026-04-10 17:31:57 -0300

    Se corrije codigo

--- Programa 01/Program.cs
+++ Programa 01/Program.cs
@@ -4,16 +4,32 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("--- SISTEMA DE REGISTRO DE USUARIO ---");
+
+            Console.Write("Por favor, ingresa tu nombre: ");
+            string nombre1 = Console.ReadLine();
+
+            Console.Write("Ingresa tu edad: ");
+            int edad1 = Int32.Parse(Console.ReadLine());
+
+            Console.Write("Ingresa tu estatura: ");
+            double estatura = double.Parse(Console.ReadLine()); 
+
+            Console.WriteLine("\n--- PERFIL CREADO ---");
+            Console.WriteLine("Nombre: " + nombre1);
+            Console.WriteLine("Edad el próximo año: " + (edad1 + 1));
+            Console.WriteLine("Estatura: " + estatura + " metros");
+
             Console.WriteLine("--- SISTEMA DE REGISTRO DE ALUMNO ---");
 
             Console.Write("Por favor, ingresa tu nombre: ");
-            string nombre = Console.ReadLine();
+            string nombre2 = Console.ReadLine();
 
             Console.Write("Por favor, ingresa tu apellido: ");
             string apellido = Console.ReadLine();
 
             Console.Write("Ingresa tu edad: ");
-            int edad = Int32.Parse(Console.ReadLine());
+            int edad2 = Int32.Parse(Console.ReadLine());
 
             Console.Write("Ingresa tu DNI: ");
             int DNI = Int32.Parse(Console.ReadLine());
@@ -24,23 +40,66 @@
             Console.Write("Por favor, ingresa tu carrera: ");
             string carrera = Console.ReadLine();
 
-            Console.Write("Por favor, ingresa tu email: ");
-            string email = Console.ReadLine();
-
-            Console.Write("Por favor, ingresa tu celular: ");
-            string celular = Console.ReadLine();
-
             Console.WriteLine("\n--- ALUMNO REGISTRADO ---");
-            Console.WriteLine("Nombre: " + nombre);
+            Console.WriteLine("Nombre: " + nombre2);
             Console.WriteLine("Apellido: " + apellido);
-            Console.WriteLine("Edad: " + edad);
+            Console.WriteLine("Edad: " + edad2);
             Console.WriteLine("Legajo: " + legajo);
             Console.WriteLine("Carrera: " + carrera);
-            Console.WriteLine("E-mail: " + email);
-            Console.WriteLine("Celular: " + celular);
+
+            Console.WriteLine("--- SISTEMA DE REGISTRO DE PRODUCTO ---");
+            Console.WriteLine("Por favor, ingresa el nombre del producto: ");
+            string nombreProducto = Console.ReadLine();
+
+            Console.WriteLine("Ingrese precio del producto: ");
+            decimal precioProducto = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el código de barras del producto: ");
+            int codigoBarras = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n--- PRODUCTO REGISTRADO ---");
+            Console.WriteLine("Nombre del producto: " + nombreProducto);
+            Console.WriteLine("Precio del producto: $" + precioProducto);
+            Console.WriteLine("Código de barras: " + codigoBarras);
+
+            Console.WriteLine("\n--- SISTEMA DE REGISTRO DE FACTURA ---");
+            Console.WriteLine("Ingrese el número de factura: ");
+            int numeroFactura = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el nombre del cliente: ");
+            string nombreCliente = Console.ReadLine();
+
+            Console.WriteLine("Ingrese el monto total de la factura: ");
+            decimal montoTotal = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n--- FACTURA REGISTRADA ---");
+            Console.WriteLine("Número de factura: " + numeroFactura);
+            Console.WriteLine("Nombre del cliente: " + nombreCliente);
+            Console.WriteLine("Monto total: $" + montoTotal);
+
+            Console.WriteLine("\n--- SISTEMA DE REGISTRO DE EMPLEADO ---");
+            Console.WriteLine("Ingrese el nombre del empleado: ");
+            string nombreEmpleado = Console.ReadLine();
+            
+            Console.WriteLine("Ingrese el apellido del empleado: ");
+            string apellidoEmpleado = Console.ReadLine();
+
+            Console.WriteLine("Ingrese la edad del empleado: ");
+            int edadEmpleado = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el número de legajo del empleado: ");
+            int legajoEmpleado = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n--- EMPLEADO REGISTRADO ---");
+            Console.WriteLine("Nombre del empleado: " + nombreEmpleado);
+            Console.WriteLine("Apellido del empleado: " + apellidoEmpleado);
+            Console.WriteLine("Edad del empleado: " + edadEmpleado);
+            Console.WriteLine("Número de legajo del empleado: " + legajoEmpleado);
+
 
             Console.WriteLine("\nPresiona ENTER para salir.");
             Console.ReadLine();
+
         }
     }
 }
--- Programa 01/obj/Debug/net10.0/Programa 01.AssemblyInfo.cs
+++ Programa 01/obj/Debug/net10.0/Programa 01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Programa 01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f87a35ee54b37fa6ac7daacd6c85425ed3be9367")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84f0d894af2c2e1b69b0c28e215ba3d3ef68f039")]
 [assembly: System.Reflection.AssemblyProductAttribute("Programa 01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Programa 01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
